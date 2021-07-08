4265058edc125e1cb0c478e2a5d08e2c27038219
Valentin Churavy <v.churavy@gmail.com>
2021-07-08 17:00:45 +0200

    avoid dependency hell

--- .ci/test.jl
+++ .ci/test.jl
@@ -3,9 +3,16 @@ import Pkg
 pkgs = [
     "KernelAbstractions",
 ]
+
+backend = get(ENV, "KERNELABSTRACTIONS_TEST_BACKEND", nothing)
+
 if !(VERSION < v"1.6-")
-    push!(pkgs, "ROCKernels")
-    push!(pkgs, "CUDAKernels")
+    if backend === nothing || backend == "ROCM"
+        push!(pkgs, "ROCKernels")
+    end
+    if backend === nothing || backend == "CUDA"
+        push!(pkgs, "CUDAKernels")
+    end
     push!(pkgs, "KernelGradients")
 end
 
